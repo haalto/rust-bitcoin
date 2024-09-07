5b7fe77a2a5278d4751b00973447af6b411a8940
Hannes Aaltonen <aaltonen.hannes@gmail.com>
2024-09-07 22:25:44 +0300

    Start implementing lib

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod crypto;
+pub mod sha256;
+pub mod types;
+pub mod util;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+use serde::{Deserialize, Serialize};
+use uint::construct_uint;
+construct_uint! {
+    #[derive(Serialize, Deserialize)]
+    pub struct U256(4);
 }
